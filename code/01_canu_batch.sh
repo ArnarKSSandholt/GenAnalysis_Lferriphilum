5fb138c2ff805d2fbf1131e45bc426924246d73f
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2019-04-04 13:39:03 +0200

    Edited Canu assembly batch file

--- code/01_canu_batch.sh
+++ code/01_canu_batch.sh
@@ -3,15 +3,19 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:00:00
-#SBATCH -J job_name
+#SBATCH -t 13:00:00
+#SBATCH -J L_ferr_Canu_assembly
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user your_email
+#SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
 
 # Load modules
 module load bioinfo-tools
-module load ....
+module load canu
 
 # Your commands
-<Command_1...>
-<Command_2...>
+canu \
+ -p lferriphilum -d lferriphilum-mix \
+ genomeSize=2.6m \
+ -pacbio-raw ERR2028*.fastq.gz \
+ stopOnReadQuality=false \
+ maxThreads=4
